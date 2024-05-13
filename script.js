53793c3e1d578ed4d392170c7bb507a5dd326775
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-05-13 12:45:17 -0300

    uptade script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+w = 30 + 50;
+w %= 3;
+alert("O valor de w é: " + (w))
+
 x = 10 + 20;
 x += 50;
-alert(x)
+alert("O valor de x é: " + (x))
 
 
 y = 50 + 20;
 y -= 20;
-alert(y)
+alert("O valor de y é: " + (y))
 
 
 z = 129 + 34
 z *= 30;
-alert(z)
+alert("O valor de z é: " + (z))
