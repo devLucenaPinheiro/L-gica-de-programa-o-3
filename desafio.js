6ebd97658d19355f558c1932d17e836d21977ae0
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-05-13 13:18:51 -0300

    update

---
+++ desafio.js
@@ -0,0 +1,10 @@
+let x = 10;
+
+x += 5;
+x *= 5;
+x++;
+alert("O resto da divisão de " + x + " por 6 é: " + (x % 6));
+alert("O quadrado do resto da divisão de " + x + " por 6 é: " + ((x % 6) ** 2));
+alert("O valor da variavel é: " + x);
+x /= 4;
+alert("O valor final da variavel é: " + x);
--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+v = 10;
+--v;
+alert(v);
+
 w = 30 + 50;
 w %= 3;
-alert("O valor de w é: " + (w))
+alert("O valor de w é: " + (w));
 
 x = 10 + 20;
 x += 50;
-alert("O valor de x é: " + (x))
+alert("O valor de x é: " + (x));
 
 
 y = 50 + 20;
 y -= 20;
-alert("O valor de y é: " + (y))
+y--;
+alert("O valor de y é: " + (y));
 
 
 z = 129 + 34
 z *= 30;
-alert("O valor de z é: " + (z))
+z++;
+alert("O valor de z é: " + (z));
