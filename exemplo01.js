87e100dcdefdbe916ec1935e074c27af06bd6346
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-05-17 20:58:21 -0300

    uptade script js

---
+++ exemplo01.js
@@ -0,0 +1,11 @@
+let mensagem = 'Olá eu sou uma "mensagem de texto"';
+console.log(mensagem);
+
+
+console.log(String(0));
+console.log(typeof 10);
+console.log(typeof 10.3);
+console.log(typeof false);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof null);
