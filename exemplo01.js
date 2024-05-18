70726fc23402d20354fc3edc3151dff960a1977d
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-05-17 21:00:54 -0300

    uptade js

--- exemplo01.js
+++ exemplo01.js
@@ -1,11 +1,20 @@
 let mensagem = 'Olá eu sou uma "mensagem de texto"';
 console.log(mensagem);
 
+let oi = String("olá");
+console.log(oi)
 
-console.log(String(0));
+console.log(typeof String(0));
+
 console.log(typeof 10);
+
 console.log(typeof 10.3);
+
 console.log(typeof false);
+
 console.log(typeof true);
+
 console.log(typeof 0);
+
 console.log(typeof null);
+
