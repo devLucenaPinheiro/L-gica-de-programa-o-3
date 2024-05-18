4aea10539a81b5af60878b71f6b1aa2f6445077b
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-05-17 21:21:44 -0300

    update exemplo
    conversão de uma variável com o tipo de número para o tipo string

--- exemplo01.js
+++ exemplo01.js
@@ -22,8 +22,20 @@ let isValid = true;
 
 console.log(typeof isValid);
 
-//valores que são convertidos para falso no boolean: strings vazias" ", número 0,(se for 0.1 já é considerado true), valor undefined, NaN e null são considerados falsos. TODO resto quando convertido para boolean é true
+//valores que são convertidos para falso no boolean: strings vazias" ", número 0,(se for 0.1 já é considerado true), valor undefined, NaN(not a number) e null(nulo) são considerados falsos. TODO resto quando convertido para boolean é true
 
-console.log(Boolean(null))
-console.log(Boolean(-2))
+console.log(Boolean(null));
+console.log(Boolean(-2));
+console.log(Boolean(0));
+console.log(Boolean(undefined));
 
+let semValor
+console.log(semValor);  //variável está definida como undefined, pois ela não tem valor.
+
+let n = parseFloat("olá");
+console.log(n);
+console.log(typeof n);  //o tipo da variável n é um número, mas ela é NaN pois não tem valor de número atribuido à ela
+
+v = 30;
+console.log(typeof v);
+console.log(typeof(v.toString()));
