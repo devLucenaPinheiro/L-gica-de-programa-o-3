ddf6fd1f9d11c5e06b56839300c4bc7bd0692373
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-05-17 21:05:28 -0300

    uptade js
    booleans

--- exemplo01.js
+++ exemplo01.js
@@ -2,7 +2,7 @@ let mensagem = 'Olá eu sou uma "mensagem de texto"';
 console.log(mensagem);
 
 let oi = String("olá");
-console.log(oi)
+console.log(oi);
 
 console.log(typeof String(0));
 
@@ -17,4 +17,13 @@ console.log(typeof true);
 console.log(typeof 0);
 
 console.log(typeof null);
+
+let isValid = true;
+
+console.log(typeof isValid);
+
+//valores que são convertidos para falso no boolean: strings vazias" ", número 0,(se for 0.1 já é considerado true), valor undefined, NaN e null são considerados falsos. TODO resto quando convertido para boolean é true
+
+console.log(Boolean(null))
+console.log(Boolean(-2))
 
