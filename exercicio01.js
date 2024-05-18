453949e22c369452b4cba9a180ea388923ae07a2
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-05-18 10:57:33 -0300

    exercicio01 js
    exercicio 01  criado e feito

--- exercicio01.js
+++ exercicio01.js
@@ -1,4 +1,8 @@
 let n1 = parseFloat(prompt("Digite um número de 0 a 10 "));
 let n2 = parseFloat(prompt("Digite outro número de 0 a 10 "));
 
-alert(n2 >= n1);
+alert("O segundo número é maior ou igual ao primeiro? " + (n2 >= n1));
+
+let n3 = parseFloat(prompt("Digite um número: "));
+let n4 = parseFloat(prompt("Digite um número: "));
+alert("A média dos números digitados é maior que 7? "  + (( (n3 + n4) / 2) >= 7 ) );
