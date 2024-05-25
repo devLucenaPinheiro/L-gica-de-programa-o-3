1ebd36da213484ea3de6ea42c102a0b93ae46f7a
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-05-24 21:05:58 -0300

    update js

--- op_comparação.js
+++ op_comparação.js
@@ -6,4 +6,6 @@ console.log(5 != 4);
 console.log("5" + 2 != 5 + 2);
 console.log("5" !== 5);
 console.log(2 >= 2);
+
+
 // SEMPRE colocar o sinal de igual depois do sinal de menor e maior ( <= >= ) colocar => é utilizado para arrow function 
