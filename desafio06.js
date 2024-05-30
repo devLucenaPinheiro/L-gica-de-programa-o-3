c599af2ab4cdc0c8e4a78b27932dfdcedff00656
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-05-29 21:19:34 -0300

    exercicio de switch case

---
+++ desafio06.js
@@ -0,0 +1,27 @@
+let n = prompt("Digite um número de 0 à 6")
+
+switch(n){
+    case "0":
+        alert("Domingo")
+        break
+    case "1":
+        alert("Segunda-feira")
+        break
+    case "2":
+        alert("Terça-feira")
+        break
+    case "3":
+        alert("Quarta-feira")
+        break
+    case "4":
+        alert("Quinta-feira")
+        break
+    case "5":
+        alert("Sexta-feira")
+        break
+    case "6":
+        alert("Sábado")
+        break
+    default:
+        alert("Digite apenas números de 0 a 6")
+}
---
+++ desafio07.js
@@ -0,0 +1,25 @@
+n = true
+
+switch(n){
+    case false:
+        alert("O tipo da variável é: " + typeof(n))
+        break
+    case true:
+        alert("O tipo da variável é: " + typeof(n))
+        break
+    case String:
+        alert("O tipo da variável é: " + typeof(n))
+        break
+    case Number:
+        alert("O tipo da variável é: " + typeof(n))
+        break
+    case null:
+        alert("O tipo da variável é: " + typeof(n))
+        break
+    case undefined:
+        alert("O tipo da variável é: " + typeof(n))
+        break
+    default:
+        alert("Valor não reconhecido")
+
+}
