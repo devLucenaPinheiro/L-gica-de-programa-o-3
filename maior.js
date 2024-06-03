8410895074d6e98253b3e3e70f7e1b1c41c6f2ed
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-06-03 14:23:26 -0300

    maior número

---
+++ maior.js
@@ -0,0 +1,8 @@
+n1 = parseFloat(prompt("Digite dois números e direi qual é maior!"))
+n2 = parseFloat(prompt("Digite o outro número"))
+
+if(n1 > n2){
+    alert("O primeiro número é maior que o segundo: " + n1)
+} else{
+    alert("O segundo número é maior que o primeiro: " + n2)
+}
