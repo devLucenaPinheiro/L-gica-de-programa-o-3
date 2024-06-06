e2bced6db3e49bc83c4980f14a265886edc557bd
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-06-06 18:14:35 -0300

    uptade js

--- par_impar.js
+++ par_impar.js
@@ -3,5 +3,7 @@ let n = parseInt(prompt("Digite um número de 0 a 10"))
 if(n == 0 || n == 2|| n == 4 || n == 6 || n == 8){
     alert("O número é par")
 } else{
+    
+    
     alert("O número é impar")
 }
