c5b6c19c165d5317256f74ad057b21c519779de1
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-06-07 21:16:06 -0300

    update js

--- desafio.js
+++ desafio.js
@@ -7,4 +7,6 @@ alert("O resto da divisão de " + x + " por 6 é: " + (x % 6));
 alert("O quadrado do resto da divisão de " + x + " por 6 é: " + ((x % 6) ** 2));
 alert("O valor da variavel é: " + x);
 x /= 4;
+
+
 alert("O valor final da variavel é: " + x);
