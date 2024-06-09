081408d53cfb07d314aa806b57efb95d05dee312
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-06-09 20:21:29 -0300

    update calc js

--- calc.js
+++ calc.js
@@ -1,4 +1,4 @@
 let n1 = parseFloat(prompt("Informe um número: "))
 let n2 = parseFloat(prompt("Informe outro número: "))
 
-alert("Os números fornecidos foram: " + n1 + " e "  + n2 + "<br/>" + "Oque vamos fazer?" )
+alert("Os números fornecidos foram: " + n1 + " e "  + n2 + " <br/> " + "Oque vamos fazer?" )
