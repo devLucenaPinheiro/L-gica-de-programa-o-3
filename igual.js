0a530a4d8cffed216e2ee4a77ce0eaae096249fb
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-06-11 17:57:27 -0300

    testando while.js

---
+++ igual.js
@@ -0,0 +1,11 @@
+let n = parseFloat(prompt("Digite um número: "))
+let contador = 0
+
+while(contador <= 1000){
+    document.write(n + " x " + contador + " = " + contador * n + "<br>")
+    contador++
+}
+
+contador = 3
+
+document.write(contador)
