57447b6bce48f53d3e7b904ac2233a0277e0c7da
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-06-18 22:57:35 -0300

    bissexto update

--- bissextox100.js
+++ bissextox100.js
@@ -3,5 +3,9 @@ for(let ano = 1004;ano <= 2024; ano += 4){
 
     let anoBissexto = ano % 100 != 0 || ano % 400 === 0
 
-    console.log(anoBissexto, ano)
+    if(anoBissexto){
+        console.log(ano, " é bissexto ")
+    } else{
+        console.log(ano, "não é bissexto")
+    }
 }
